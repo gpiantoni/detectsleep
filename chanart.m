bcbbe71df0eb55d41a99687860f3a17cb9004f5d
Gio <github@gpiantoni.com>
2012-04-19 16:38:05 +0200

    use threshold from cfg, not complicated function

--- chanart.m
+++ chanart.m
@@ -1,14 +1,22 @@
 function [data, output] = chanart(cfg, data)
+%CHANART reject and repair single channels
+%
+% CFG
+%  .sleepepochs.chanart.auto(1).met: method to automatically find bad
+%                                    channels ('var' 'range' 'diff')
+%  .sleepepochs.chanart.auto(1).thr: in microvolts (10000, 3000, 1000)
+%
+
 
 %--------------------------%
 %-reject channels with difference methods
-output = [];
+output = '';
 
-for a = 1:numel(cfg.badchan.auto)
+for a = 1:numel(cfg.sleepepochs.chanart.auto)
   
   %------------------%
   %-automatic (above threshold in variance)
-  switch cfg.badchan.auto(a).met
+  switch cfg.sleepepochs.chanart.auto(a).met
     case 'var'
       %-------%
       %-compute var
@@ -36,7 +44,7 @@ for a = 1:numel(cfg.badchan.auto)
   %-find badchan and output
   %-------%
   %-define bad channels
-  i_bad = feval(eval(cfg.badchan.auto(a).fun), allchan);
+  i_bad = find(allchan > cfg.sleepepochs.chanart.auto(a).thr);
   badchan{a} = data.label(i_bad);
   %-------%
   
@@ -46,11 +54,11 @@ for a = 1:numel(cfg.badchan.auto)
     [~, s_bad] = sort(allchan(i_bad), 'descend');
     badname = '';
     for b = s_bad'
-      badname = sprintf('%s %s (%6.f)', badname, badchan{a}{b}, allchan(i_bad(b)));
+      badname = sprintf('%s %s (% 6d)', badname, badchan{a}{b}, allchan(i_bad(b)));
     end
     
-    outtmp = sprintf('    %s (%s): min % 5.2f, median % 5.2f, mean % 5.2f, std % 5.2f, max % 5.2f\n    %g channels were bad: %s\n\n', ...
-      cfg.badchan.auto(a).met, cfg.badchan.auto(a).fun, min(allchan), median(allchan), mean(allchan), std(allchan), max(allchan), ...
+    outtmp = sprintf('    %s (% 6d): min % 5.2f, median % 5.2f, mean % 5.2f, std % 5.2f, max % 5.2f\n    %g channels were bad: %s\n\n', ...
+      cfg.sleepepochs.chanart.auto(a).met, cfg.sleepepochs.chanart.auto(a).thr, min(allchan), median(allchan), mean(allchan), std(allchan), max(allchan), ...
       numel(badchan{a}), badname);
     output = [output outtmp];
   end
@@ -61,7 +69,7 @@ end
 %--------------------------%
 
 %-----------------%
-%-do not repair channels 
+%-do not repair channels
 if isempty(output)
   return
 end
@@ -71,27 +79,26 @@ end
 %-all bad and check if the reference is bad
 allbad = cat(1, badchan{:});
 output = numel(unique(allbad));
-
-% if ~any(strcmp('all', cfg.reref.refchannel))
-%   
-%   badref = intersect(cfg.reref.refchannel, allbad);
-%   
-%   if ~isempty(badref)
-%     badrefstr = sprintf(' %s,', badref{:});
-%     outtmp = sprintf('  WARNING: Reference channel (%s) is bad\n', badrefstr);
-%     output = [output outtmp];
-%   end
-%   
-% end
 %-----------------%
 
 %-----------------%
 %-repair channels
-load(cfg.elecfile, 'elec', 'nbor')
+%-------%
+%-create neighbors from file
+sens = ft_read_sens(cfg.sens.file);
+sens.label = upper(sens.label);
+
+cfg1 = [];
+cfg1.elec = sens;
+cfg1.method = 'distance';
+cfg1.neighbourdist = cfg.sens.dist;
+neigh = ft_prepare_neighbours(cfg1);
+%-------%
+
 cfg1 = [];
 cfg1.badchannel = allbad;
-cfg1.neighbours = nbor;
-data.elec = elec;
+cfg1.neighbours = neigh;
+data.elec = sens;
 [data] = ft_channelrepair(cfg1, data);
 data = rmfield(data, 'elec');
 %-----------------%
